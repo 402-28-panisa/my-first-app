eed9fa118013d7750736d012909dd7e619f69893
402-28-panisa <54402@yupparaj.ac.th>
2026-08-19 15:20:35 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -56,14 +56,14 @@ def show_result_dialog(ans1, ans2):
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 4
     else:
-        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
      # ตรวจข้อ 4
     if u_ans4 == "Phone":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 4
     else:
-        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
+        st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
         
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
